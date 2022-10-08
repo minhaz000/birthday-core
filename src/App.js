cf91b37ee8130d5ed0f69c181c82e253ce019096
minhaz000 <mrmminhaz@gamail.com>
2022-10-08 07:10:12 +0600

    this is first commmit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import { useTransition, animated } from 'react-spring'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
+import Sutter from './components/Sutter'
+import Timer from './components/Timer'
 
-function App() {
+export default function App() {
+const [status , setStatus] = useState(true)
+const transitions = useTransition(status, {
+  from: { x: 0 , y:1000 , opacity: 1 },
+  enter: { x:0 , y:0 , opacity: 1 },
+  leave: {x: 0 , y:-1000 , opacity: 1 },
+  delay: 500,
+ 
+})
+const [clock,setClock] =useState(50)
+const getClock =(data)=>{setClock(data); if(clock===59){ setStatus(false)  }}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className=' '>
+    
+<Timer getClock={getClock} > </Timer>
 
-export default App;
+  {transitions(( style, item) => (
+        item?<animated.div style={style} className="item"> 
+                                      <Sutter count={clock}> </Sutter>
+          </animated.div> : 
+          <animated.div style={style} className="item"> 
+                                      <Header> </Header>
+                                      <MainContent > </MainContent>
+                                      <Footer></Footer>
+          
+          </animated.div>
+  ))}
+ {/* <Sutter count={clock}> </Sutter> */}
+
+    </div>
+  )
+}
